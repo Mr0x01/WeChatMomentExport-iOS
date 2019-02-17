9d608667f6ff27dc8bdabdc11776e1c6d72ca764
WhoAmI <SlienceMakeBigMoney@Git.com>
2019-02-17 10:50:18 +0800

    初始化

---
+++ WeChatMomentExport/WeChatMomentExport/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeChatMomentExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
